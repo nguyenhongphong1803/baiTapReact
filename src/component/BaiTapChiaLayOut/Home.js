130d5231d29ed3f3caf2bb6354548a816c1ec8a2
HongPhongNguyen <nguyenhongphong1803@gmail.com>
2022-12-19 22:07:45 +0700

    upload bai tap react

---
+++ src/component/BaiTapChiaLayOut/Home.js
@@ -0,0 +1,35 @@
+import React, { Component } from "react";
+import Content from "./Content";
+import Footer from "./Footer";
+import HeaderDemo from "./HeaderDemo";
+import Navigation from "./Navigation";
+
+export default class Home extends Component {
+  render() {
+    let style = {
+      padding: 0,
+    };
+    return (
+      <div className="container-fluid">
+        <div className="row">
+          <div style={style} className="col-12">
+            <HeaderDemo />
+          </div>
+        </div>
+        <div className="row">
+          <div style={style} className="col-4">
+            <Navigation></Navigation>
+          </div>
+          <div style={style} className="col-8">
+            <Content></Content>
+          </div>
+        </div>
+        <div className="row">
+          <div style={style} className="col-md-12">
+            <Footer></Footer>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
